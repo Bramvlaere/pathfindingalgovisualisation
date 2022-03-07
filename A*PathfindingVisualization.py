c5539fddb7ad1619ba4424744244149dba6e2b7c
Bramvlaere <Bram.vanlaere@gmail.com>
2022-03-07 16:59:36 -0500

    Create A*PathfindingVisualization.py

---
+++ A*PathfindingVisualization.py
@@ -0,0 +1,180 @@
+from turtle import width
+import pygame
+import math
+from queue import PriorityQueue
+
+#defining the frame where are going to be working in
+
+f_width=800
+winframe=pygame.display.set_mode((f_width,f_width))
+pygame.display.set_caption('A* Pathfinding Visualization')
+
+
+#defining colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+
+#defining the visulisation tool
+
+class Node:
+
+    #class to determine the state of the spot
+    def __init__(self,row,col,width,total_rows):
+        self.row=row
+        self.col=col
+        self.x=row*width
+        self.y=col*width
+        self.color=WHITE
+        self.neigh=[]
+        self.width=width
+        self.total_rows=total_rows
+
+    def getpos(self):
+        return self.row,self.col
+
+
+    def is_closed(self):#has this already been done or not
+        return self.color==RED
+
+    def is_open(self):
+        return self.color==GREEN
+
+
+    def is_obstacle(self):
+        return self.color==BLACK
+
+    def is_start(self):
+        return self.color==ORANGE
+
+    def is_end(self):
+        return self.color==TURQUOISE
+
+    def reset(self):
+        return self.color==WHITE
+
+
+    def make_closed(self):
+        self.color=RED
+    def make_start(self):
+        self.color=ORANGE
+
+    def make_open(self):
+         self.color=GREEN
+
+    def make_barrier(self):
+        self.color=BLACK
+
+    def make_end(self):
+        self.color=TURQUOISE
+
+    def make_path(self):
+        self.color=PURPLE
+
+    def draw(self,winframe):
+        pygame.draw.rect(winframe,self.color,(self.x,self.y,self.width,self.width))
+
+    def update_neigh(self,grid):
+
+        pass
+    def __lt__(self,other):
+        return False #compare spots
+
+
+def h(p1,p2):
+    x1,y1=p1
+    x2,y2=p2
+    return abs(x1-x2)+abs(y1-y2)
+def make_grid(rows,width):
+    grid=[]
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node=Node(i,j,gap,rows)
+            grid[i].append(node)
+
+    return grid
+
+def draw_grid(win, rows, width):
+	gap = width // rows
+	for i in range(rows):
+		pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+		for j in range(rows):
+			pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+
+
+def draw(win, grid, rows, width):
+	win.fill(WHITE)
+
+	for row in grid:
+		for spot in row:
+			spot.draw(win)
+
+	draw_grid(win, rows, width)
+	pygame.display.update()
+
+def get_click_pos(pos,rows,width):
+    gap=width//rows 
+    y,x=pos
+    row=y//gap
+    col=x//gap
+    return row,col
+
+def main(winframe, width):# the core struture of the program
+    rows=50 #affects how fast the algo will work bigger grid means more time
+    grid=make_grid(rows,width) #function defined above
+    start=None #variables that will help to keep track of the state of the program
+    end=None
+    run=True
+    started=False
+    while run:
+        draw(winframe,grid,rows,width)
+        for event in pygame.event.get():#lets loop through all the events while run is true and check what the events are
+            if event.type==pygame.quit:
+                run=False
+            if started: # once we start the algo the user can not press anything unless quit 
+                continue
+            if pygame.mouse.get_pressed()[0]:#if the pygame mouse was left pressed
+                pos=pygame.mouse.get_pos()
+                row,col=get_click_pos(pos,rows,width) 
+                node=grid[row][col]
+                if not start: 
+                    start=node
+                    start.make_start()
+                elif not end:
+                    end=node
+                    end.make_end()
+                elif node != end and node !=start:
+                    node.make_barrier()
+
+            elif pygame.mouse.get_pressed()[2]:#if the pygame mouse was right pressed
+                pass
+                 
+    pygame.quit()
+
+main(winframe,f_width)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
